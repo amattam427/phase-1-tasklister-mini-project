6a694cc6dd92c03da0f21ae7e914213735d270b0
Ashley Mattam <amattam427@gmail.com>
2021-12-02 02:10:15 -0500

    completed

--- README.md
+++ README.md
@@ -37,7 +37,7 @@ moving forward. If you take a look at the `index.html` file, you'll see that the
 
 ### Deliverables
 
-* As a user, I should be able to type a task into the input field.
+* As a user, I should be able to type a task into the input field.--
 * As a user, I should be able to click some form of a submit button.
 * As a user, I expect to see the task string that I provided appear in the DOM
   after the submit button has been activated.
--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm =  document.querySelector('#create-task-form');
+  //insert value into DOM
+  const taskList = document.querySelector('#tasks')
+  const taskData = document.querySelector('new-task-description');
+
+  taskForm.addEventListener('submit', (e)=> {
+    const task = taskData.value;
+    e.preventDefault();
+    const li = document.createElement('li');
+    li.innerText = task;
+    const button = document.createElement('button');
+    button.innerText = 'X';
+    li.appendChild(button);
+    taskList.appendChild(li);
+    taskData.value = '';
+    button.addEventListener('click', (e) => {
+      e.target.parentNode.remove();
+    });
+  });
 });
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
